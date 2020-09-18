c81f9f2a07e834fcde773aa9a4c80480a7c7fb77
Kostya Ryabinin <kostya@nulana.com>
2020-09-18 08:03:00 +0500

    soc example, work in progress

--- scivi.py
+++ scivi.py
@@ -36,6 +36,12 @@ def glove_page():
     srv = SciViServer(Onto.load_from_file("kb/glove/glove.merged.ont"), None, Mode.VISUALIZATION)
     return send_from_directory("client", "editor.html"), 200, {'Content-Type': 'text/html; charset=utf-8'}
 
+@app.route("/soc")
+def soc_page():
+    global srv
+    srv = SciViServer(Onto.load_from_file("kb/soc/soc.merged.ont"), None, Mode.VISUALIZATION)
+    return send_from_directory("client", "editor.html"), 200, {'Content-Type': 'text/html; charset=utf-8'}
+
 @app.route("/scivi-editor-main.js")
 def editor_main():
     global srv
