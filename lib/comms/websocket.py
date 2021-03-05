4439e80fa14a00a2b8b5776d3abf7aeea8fdf6a7
Kostya Ryabinin <kostya@nulana.com>
2021-03-05 09:42:26 +0500

    server: fixes for web socket

--- lib/comms/websocket.py
+++ lib/comms/websocket.py
@@ -7,43 +7,57 @@ import json
 from threading import Thread, Lock
 
 
-async def ws(websocket, path):
-    while True:
-        data = GLOB["WebSocketThread"].get_message()
-        if data:
-            try:
-                await websocket.send(data)
-            except:
-                break
-        else:
-            await asyncio.sleep(0)
-
 class WSThread(Thread):
     def __init__(self):
         self.mutex = Lock()
         self.queue = []
         self.loop = None
-        self.wsServer = None
+        self.stopper = None
+        self.wsRunning = False
         Thread.__init__(self)
+
+    async def ws(self, websocket, path):
+        print("> WebSocket server started")
+        self.set_ws_running(True)
+        while True:
+            data = GLOB["WebSocketThread"].get_message()
+            if data:
+                try:
+                    await websocket.send(data)
+                except:
+                    print("> WebSocket closed")
+                    break
+            else:
+                await asyncio.sleep(0)
+        self.set_ws_running(False)
+        print("> WebSocket server stopped")
+
+    async def ws_run(self, stop):
+        async with websockets.serve(self.ws, "127.0.0.1", 5001):
+            await stop
 
     def run(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
-        wsServerWrapper = websockets.serve(ws, "127.0.0.1", 5001)
-        self.wsServer = wsServerWrapper.ws_server
-        asyncio.get_event_loop().run_until_complete(wsServerWrapper)
-        asyncio.get_event_loop().run_forever()
+        self.stopper = self.loop.create_future()
+        self.loop.run_until_complete(self.ws_run(self.stopper))
 
     def stop_internal(self):
-        self.wsServer.close()
-        self.loop.stop()
+        self.stopper.set_result(None)
 
     def stop(self):
         self.loop.call_soon_threadsafe(self.stop_internal)
+        self.join()
+
+    def set_ws_running(self, running):
+        self.mutex.acquire()
+        self.wsRunning = running
+        self.mutex.release()
 
     def put_message(self, msg):
         self.mutex.acquire()
-        self.queue.append(msg)
+        if self.wsRunning:
+            self.queue.append(msg)
         self.mutex.release()
 
     def get_message(self):
@@ -62,9 +76,10 @@ if "WebSocketThread" in GLOB:
 else:
     wsThread = WSThread()
     GLOB["WebSocketThread"] = wsThread
-    REGISTER_COROUTINE(wsThread, wsThread.stop)
+    REGISTER_SUBTHREAD(wsThread, wsThread.stop)
     wsThread.start()
 
 tx = INPUT["TX"]
 if tx:
+    # TODO: do not append to queue if ws is dead
     wsThread.put_message({ SETTINGS_VAL["Node Address"]: tx })
