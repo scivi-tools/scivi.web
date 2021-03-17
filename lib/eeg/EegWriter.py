09c5fdc91635947313b77fc523c3e608c62a5beb
LinguoLab <linguolab@psu.ru>
2021-03-17 20:59:56 +0500

    Fix writer

--- lib/eeg/EegWriter.py
+++ lib/eeg/EegWriter.py
@@ -1,41 +1,43 @@
 #!/usr/bin/false
 
 from datetime import datetime
+import numpy as np
 
 import cyrtranslit
 import pyedflib
+from random import random
 
 class EegWriter(object):
-    def __init__(self, filename, iteration, date, time, informant_code, directory="."):
-        self.id = EegWriter.compute_id(filename, iteration, date, time, informant_code)
+    def __init__(self, filename, iteration, time, informant_code, directory="."):
+        self.wid = EegWriter.compute_id(filename, iteration, time, informant_code)
 
         # TODO: add date/time?
-        self.filename = "{}/{}_{}.edf".format(directory, iteration, filename)
+        self.filename = "{}/{:04}_{}_{:09}.edf".format(directory, iteration, filename, int(random()*10000000))
 
         self.write_edf = pyedflib.highlevel.write_edf
 
-        self.header = self.create_header(filename, iteration, date, time, informant_code)
+        self.header = self.create_header(filename, iteration, time, informant_code)
 
         self.signal_headers = []
 
-        self.data = []
+        self.data = {}
 
-    def compute_id(filename, iteration, date, time, informant_code):
-        return hash("_".join(filename, iteration, date, time, informant_code))
+    def compute_id(filename, iteration, time, informant_code):
+        return hash("_".join([filename, str(iteration), str(time), informant_code]))
 
-    def create_header(self, filename, iteration, date, time, informant_code):
+    def create_header(self, filename, iteration, time, informant_code):
         return {
             # TODO: this fields should ALL be accessible in the interface
             "technician"          : "SciVi-EEG",
-            "recording_additional": "{}_{}".format(iteration, self.clean_string(filename)),
+            "recording_additional": "{:04}_{}".format(iteration, self.clean_string(filename)),
             "patientname"         : "X", # Depersonified
             "patient_additional"  : "X", # Depersonified
             "patientcode"         : "SCE-"+self.clean_string(str(informant_code)),
             "equipment"           : "EBNeuro BE Plus LTM",
             "admincode"           : "X", # TODO: Unknown
             "gender"              : "X", # Depersonified
-            "startdate"           : self.format_date(date, time),
-            "birthdate"           : "X", # Depersonified
+            "startdate"           : time,
+            "birthdate"           : "", # Depersonified
         }
 
     def create_signal_header(self, label):
@@ -44,29 +46,35 @@ class EegWriter(object):
         return {
             'label'        : label,    # channel label (string, <= 16 characters, must be unique)
             'dimension'    : "uV",     # physical dimension (e.g., mV) (string, <= 8 characters)
+            'transducer'   : "AgAl",   # 
+            'prefilter'    : "50 Hz",  # 
             'sample_rate'  : 128,      # sample frequency in hertz (int)
             'physical_max' : +4096.0,  # maximum physical value (float)
             'physical_min' : -4096.0,  # minimum physical value (float)
-            'digital_max'  : -2**15,   # maximum digital value (int, -2**15 <= x < 2**15)
-            'digital_min'  :  2**15-1, # minimum digital value (int, -2**15 <= x < 2**15)
+            'digital_max'  :  2**15-1, # maximum digital value (int, -2**15 <= x < 2**15)
+            'digital_min'  : -2**15,   # minimum digital value (int, -2**15 <= x < 2**15)
         }
 
     def clean_string(self, string):
         return cyrtranslit.to_latin(string.replace(" ", "_"), 'ru')
 
-    def format_date(self, date, time):
-        return datetime.combine(date, time)
-
     def write(self, data):
         if not self.signal_headers:
             for label in data[0]:
                 self.signal_headers.append(self.create_signal_header(label))
+                self.data[label] = []
 
-        self.data.append(data[1])
+        for label, signal in zip(data[0], data[1]):
+            self.data[label].append(signal)
 
     def close(self):
-        if len(data) < 1:
+        if len(self.data) < 1:
             return
 
-        self.write_edf(self.filename, self.data, self.signal_headers, self.header, digital=False)
+        sorted_data = [np.hstack(self.data[key]) for key in sorted(self.data)]
+        sorted_headers = list(sorted(self.signal_headers, key=lambda x: x['label']))
+
+        sorted_data = np.array(sorted_data, dtype=np.int32).reshape( (len(sorted_data), -1) )
+
+        self.write_edf(self.filename, sorted_data, sorted_headers, self.header, digital=True)
         
