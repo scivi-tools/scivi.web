5b37e1948bd4a4fc99ec0adcb843e8bbb02b7a38
Kostya Ryabinin <kostya@nulana.com>
2021-04-08 13:26:00 +0500

    eeg: fix for channel test

--- lib/eeg/testchannel.py
+++ lib/eeg/testchannel.py
@@ -4,12 +4,14 @@
 import math
 
 
+THRESHOLD = 10000
+
 isHigh = False
 if "EEG" in INPUT:
     chName = SETTINGS_VAL["Channel Name"]
     eeg = INPUT["EEG"]
     if chName in eeg[0]:
         val = eeg[1][eeg[0].index(chName)]
-        isHigh = math.abs(val) > 10000
+        isHigh = math.abs(val[0]) > THRESHOLD or math.abs(val[-1]) > THRESHOLD
 
 OUTPUT["Is High"] = isHigh
