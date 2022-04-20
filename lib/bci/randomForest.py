79ad2adc307ed9d8149f62a6ed4a7954e501693a
Dmitry <chiruhind@gmail.com>
2022-04-20 23:47:05 +0500

    added accumulator

--- lib/bci/randomForest.py
+++ lib/bci/randomForest.py
@@ -1,4 +1,4 @@
-#import joblib
+import joblib
 import numpy as np
 
 RF_MODE_TRAIN = 0
@@ -19,27 +19,27 @@ RF_KEY = 'RF'
 def p(x):
     return "{}_{}".format(MODULE_PREFIX, x)
 
-features = INPUT[RF_INPUT_FEATURES]
-labels = INPUT[RF_INPUT_LABELS]
-np.savetxt("features.csv", features, delimiter=",")
-np.savetxt("labels.csv", np.array([0 if i == 'relax' else 1 for i in labels]), delimiter=",")
+if RF_INPUT_FEATURES in INPUT:
+    features = INPUT[RF_INPUT_FEATURES]
 
-mode = int(SETTINGS_VAL[RF_SETTING_MODE])
-model_file = SETTINGS_VAL[RF_SETTING_MODEL]
+    mode = int(SETTINGS_VAL[RF_SETTING_MODE])
+    model_file = SETTINGS_VAL[RF_SETTING_MODEL]
 
-rf = GLOB.get(p(RF_KEY))
+    rf = GLOB.get(p(RF_KEY))
 
-if not rf:
-    from sklearn.ensemble import RandomForestClassifier
-    rf = RandomForestClassifier()
-    GLOB[p(RF_KEY)] = rf
+    if rf is None:
+        from sklearn.ensemble import RandomForestClassifier
+        rf = RandomForestClassifier()
+        GLOB[p(RF_KEY)] = rf
 
-if mode == RF_MODE_TRAIN:
-    labels = INPUT[RF_INPUT_LABELS]
-    rf.fit(features, labels)
-    joblib.dump(rf, model_file)
-elif mode == RF_MODE_EVAL:
-    rf = joblib.load(model_file)
-    OUTPUT[RF_OUTPUT_RESULT] = rf.predict(features)
-else:
-    raise ValueError()
+    if mode == RF_MODE_TRAIN:
+        labels = INPUT[RF_INPUT_LABELS]
+        rf.fit(features, labels)
+        joblib.dump(rf, model_file)
+        print('Model saved')
+    elif mode == RF_MODE_EVAL:
+        rf = joblib.load(model_file)
+        OUTPUT[RF_OUTPUT_RESULT] = rf.predict(features)
+        print(OUTPUT[RF_OUTPUT_RESULT])
+    else:
+        raise ValueError()
