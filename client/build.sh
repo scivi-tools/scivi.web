7fa1aaf952c82d4d1d278f9048662e6a0c441f43
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2022-04-21 11:12:42 +0500

    client: fix for build script

--- client/build.sh
+++ client/build.sh
@@ -5,7 +5,7 @@ UGLIFY="./node_modules/.bin/uglifyjs"
 
 mkdir -p lib
 
-if [ $1 == "debug" ]; then
+if [[ $1 == "debug" ]]; then
     $BROWSERIFY src/scivi-editor.js --standalone SciViEditor -o lib/scivi-editor.min.js
 else
     $BROWSERIFY src/scivi-editor.js --standalone SciViEditor -o lib/scivi-editor.tmp.js
