4f31ffe0b08934224b5de95cd91cb2328f860d45
John <goldnotch@mail.ru>
2022-05-16 10:40:28 +0500

    move from Uglify-JS to Terser https://www.npmjs.com/package/terser

--- client/build.sh
+++ client/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BROWSERIFY="./node_modules/.bin/browserify"
-UGLIFY="./node_modules/.bin/uglifyjs"
+TERSER="./node_modules/.bin/terser"
 
 mkdir -p lib
 
@@ -9,6 +9,6 @@ if [ $1 == "debug" ]; then
     $BROWSERIFY src/scivi-editor.js --standalone SciViEditor -o lib/scivi-editor.min.js
 else
     $BROWSERIFY src/scivi-editor.js --standalone SciViEditor -o lib/scivi-editor.tmp.js
-    $UGLIFY lib/scivi-editor.tmp.js -o lib/scivi-editor.min.js
+    $TERSER lib/scivi-editor.tmp.js -o lib/scivi-editor.min.js
     rm lib/scivi-editor.tmp.js
 fi
