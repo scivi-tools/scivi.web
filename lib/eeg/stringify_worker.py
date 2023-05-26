cea52a0a96e7240d6243723dc8bf8531851cb61b
LinguoLab <linguolab@psu.ru>
2023-05-26 14:18:49 +0500

    Two utility operators

---
+++ lib/eeg/stringify_worker.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+STRINGIFY_INPUT_VALUE = 'Numeric Value'
+STRINGIFY_OUTPUT_VALUE = 'String Value'
+
+if MODE == 'INITIALIZATION':
+    pass
+else:
+    OUTPUT[STRINGIFY_OUTPUT_VALUE] = str(INPUT[STRINGIFY_INPUT_VALUE])
+
+PROCESS()
