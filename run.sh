df6527124820bdbfa1ff2819e07140d9ca7e92ac
Konstantin Riabinin <konstantin.riabinin@uni-heidelberg.de>
2025-01-03 12:23:22 +0100

    venv support

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@
 
 export FLASK_APP=scivi.py
 python3 -m flask run --host=0.0.0.0 --port=5555
+if [ $? -ne 0 ]; then
+    echo "Maybe you are out of python virtual environment"
+    echo "Try:"
+    echo "./setup.py"
+    echo "source .venv/bin/activate"
+fi
